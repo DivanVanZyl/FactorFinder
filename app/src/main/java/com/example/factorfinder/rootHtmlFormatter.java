2387695a109bd26a13c4d0475cffcf575aad329f
divan <divanvanzyl@gmail.com>
2020-04-16 00:52:23 +0200

    Formatted output to convert to int when necessary.

--- app/src/main/java/com/example/factorfinder/rootHtmlFormatter.java
+++ app/src/main/java/com/example/factorfinder/rootHtmlFormatter.java
@@ -12,7 +12,24 @@ public class rootHtmlFormatter {
     public String getHTMLText(int numerator, int denominator) {
         return "<sup>" + numerator + "</sup>/<sub>" + denominator + "</sub>";
     }
+
     public String getHTMLText(double numerator, double denominator) {
-        return "<sup>" + numerator + "</sup>/<sub>" + denominator + "</sub>";
+
+        String num = Double.toString(numerator);
+        String denom = Double.toString(denominator);
+
+        if(numerator%1 == 0)
+        {
+            int numInt = (int) numerator;
+            num = Integer.toString(numInt);
+        }
+        if(denominator%1 == 0)
+        {
+
+            int denomInt = (int) denominator;
+            denom = Integer.toString(denomInt);
+        }
+
+        return "<sup>" + num + "</sup>/<sub>" + denom + "</sub>";
     }
 }
